21074d7261946330f7a2f202d1fbad1675ade7ae
Niels Weistra <niels.weistra@gmail.com>
2018-06-27 02:36:26 +0200

    Addition to last commit

--- Deploy-Requirements.ps1
+++ Deploy-Requirements.ps1
@@ -19,8 +19,6 @@ Param(
     [string]$ServicePrincipalName
 )
 
-function Main {
-
     $RandomPassword = ([char[]]([char]33..[char]95) + ([char[]]([char]97..[char]126)) + 0..9 | Sort-Object {Get-Random})[0..32] -join ''
     $Password = ConvertTo-SecureString $RandomPassword -AsPlainText -Force
 
@@ -29,10 +27,6 @@ function Main {
     Select-AzureRmSubscription -SubscriptionId $SubscriptionID
     $ServicePrincipal = New-ServicePrincipal -Password $Password -DisplayName $ServicePrincipalName -HomePage "http://$(-join ((65..90) + (97..122) | Get-Random -Count 5 | % {[char]$_}))" -IdentifierUris "http://$(-join ((65..90) + (97..122) | Get-Random -Count 5 | % {[char]$_}))"
     
-    New-KeyVault -VaultName $VaultName -ResourceGroup $ResourceGroup -Region $Region -Company $Company -Enviroment $Enviroment -ServicePrincipalName $ServicePrincipal.DisplayName[0]
-    New-Secret -VaultName $VaultName -Name $ServicePrincipalName -SecretValue $RandomPassword
-    Get-Secret -VaultName $VaultName -Name $ServicePrincipalName -SecretValue $RandomPassword
-    
-}
-
-Main
+    New-KeyVault -VaultName $VaultName -ResourceGroup $ResourceGroup -Region $Region -Company $Company -Enviroment $Enviroment -ServicePrincipal $ServicePrincipal
+    New-Secret -VaultName $VaultName -Name $ServicePrincipalName -SecretValue $Password
+    Get-Secret -VaultName $VaultName -Name $ServicePrincipalName
