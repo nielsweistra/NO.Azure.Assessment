7294efdeebd679768ae45f0d66b22fac99773279
Niels Weistra <niels.weistra@gmail.com>
2018-06-27 23:50:41 +0200

    Reorganized parameters

--- Deploy-AzureTemplate.ps1
+++ Deploy-AzureTemplate.ps1
@@ -1,32 +1,24 @@
 Using module ".\Modules\NO.PowershellAzure\NO.PowershellAzure.psm1"
 [CmdletBinding()]
 Param(
-    [parameter(Mandatory=$true)]
-    [string]$TenantID,
-    [parameter(Mandatory=$true)]
-    [string]$SubscriptionID,
-    [parameter(Mandatory=$true)]
-    [securestring]$AdminPassword,
-    [parameter(Mandatory=$true)]
-    [string]$ServicePrincipal,
-    [parameter(Mandatory=$true)]
-    [securestring]$ServicePrincipalPassword,
-    [parameter(Mandatory=$false)]
-    [string]$ResourceGroup = "NO_RG",
-    [string]$Region = "WestEurope",
-    [string]$ArmTemplate = ".\Templates\azuredeploy.json",
-    [string]$ArmTemplateParameters = ".\Templates\azuredeploy.parameters.json",
-    [string]$Company = "NO",
-    [string]$Enviroment = "Test",
-    [string]$DeployLabel
+    [parameter(Mandatory=$true)] [string]$TenantID,
+    [parameter(Mandatory=$true)] [string]$SubscriptionID,
+    [parameter(Mandatory=$false)] [securestring]$AdminPassword,
+    [parameter(Mandatory=$true)] [string]$ServicePrincipal,
+    [parameter(Mandatory=$true)] [securestring]$ServicePrincipalPassword,
+    [parameter(Mandatory=$false)] [string]$ResourceGroup = "NO_RG",
+    [parameter(Mandatory=$false)] [string]$Region = "WestEurope", 
+    [parameter(Mandatory=$false)] [string]$ArmTemplate = ".\Templates\azuredeploy.json",
+    [parameter(Mandatory=$false)] [string]$ArmTemplateParameters = ".\Templates\azuredeploy.parameters.json",
+    [parameter(Mandatory=$false)] [string]$Company = "NO",
+    [parameter(Mandatory=$false)] [string]$Enviroment = "Test",
+    [parameter(Mandatory=$false)] [string]$DeployLabel
 )
 
 $DeployLabel = "$($Company)-$($Enviroment)-$($ResourceGroup)-$(Get-Date -Date (Get-Date -format "dd-MMM-yyyy HH:mm") -UFormat %s)"
 
-
-
-$psCred = New-Object System.Management.Automation.PSCredential($ServicePrincipal, $ServicePrincipalPassword)
-Add-AzureRmAccount -Credential $psCred -ServicePrincipal -TenantId $TenantID
+$spCred = New-Object System.Management.Automation.PSCredential($ServicePrincipal, $ServicePrincipalPassword)
+Add-AzureRmAccount -Credential $spCred -ServicePrincipal -TenantId $TenantID
 
 Select-AzureRmSubscription -SubscriptionId $SubscriptionID
 
