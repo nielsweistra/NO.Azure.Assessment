cc609f0604313d2e771a0e5257eb0ad6f930158b
Niels Weistra <niels.weistra@gmail.com>
2018-06-28 02:00:49 +0200

    Implement scripts into module as functions

--- Modules/NO.PowershellAzure/NO.PowershellAzure.psm1
+++ Modules/NO.PowershellAzure/NO.PowershellAzure.psm1
@@ -29,47 +29,59 @@ class AzureRestServiceManager {
 
     [void] Get ([uri]$ApiUri) {
         $Headers = @{}
-        $Headers.Add("Authorization","$($this.Token.token_type) "+ " " + "$($this.Token.access_token)")
+        $Headers.Add("Authorization", "$($this.Token.token_type) " + " " + "$($this.Token.access_token)")
         $this.Response = Invoke-RestMethod -Method Get -Uri $ApiUri -Headers $Headers
     }
 
-    [void] Put ([uri]$ApiUri,[string]$body) {
+    [void] Put ([uri]$ApiUri, [string]$body) {
         $contentType = "application/json"  
         $Headers = @{}
-        $Headers.Add("Authorization","$($this.Token.token_type) "+ " " + "$($this.Token.access_token)")
+        $Headers.Add("Authorization", "$($this.Token.token_type) " + " " + "$($this.Token.access_token)")
         $this.Response = Invoke-RestMethod -Method Put -Uri $ApiUri -Headers $Headers -Body $body -ContentType $contentType
     }
 }
 
-function New-Secret  {
-    [CmdletBinding()]
+Function New-RandomKey() {
+
+    $key = New-Object byte[](32)
+    $rng = [System.Security.Cryptography.RNGCryptoServiceProvider]::Create()
+    $rng.GetBytes($key)
+
+    return [System.Convert]::ToBase64String($key)
+}
+
+function New-AdminPassword ($Length) {
+    If (-not($Length)) {
+        $Length = 16
+    }
+    $Password = ConvertTo-SecureString $(([char[]]([char]33..[char]95) + ([char[]]([char]97..[char]126)) + 0..16 | Sort-Object {Get-Random})[0..$Length] -join '') -AsPlainText -Force 
+ 
+    return $Password
+}
+
+function New-Secret {
     Param (
-    [parameter(Mandatory=$true)]
-    $VaultName,
-    [parameter(Mandatory=$true)]
-    $Name,
-    [parameter(Mandatory=$true)]
-    $SecretValue
+        [parameter(Mandatory = $true)] [string] $VaultName,
+        [parameter(Mandatory = $true)] [string] $Name,
+        [parameter(Mandatory = $true)] [securestring] $SecretValue
     )
 
-    if(-not(Get-AzureKeyVaultSecret -vaultName $VaultName -name $Name)) {
+    if (-not(Get-AzureKeyVaultSecret -vaultName $VaultName -name $Name)) {
 
-        $secret = Set-AzureKeyVaultSecret -VaultName $VaultName -Name $Name -SecretValue $SecretValue
+        $Secret = Set-AzureKeyVaultSecret -VaultName $VaultName -Name $Name -SecretValue $SecretValue
         Write-Verbose (get-AzureKeyVaultSecret -vaultName $VaultName -name $Name).SecretValueText
 
     }
-    
+    return $Secret
 }
 
 function Get-Secret {
     Param (
-    [parameter(Mandatory=$true)]
-    $VaultName,
-    [parameter(Mandatory=$true)]
-    $Name
+        [parameter(Mandatory = $true)] $VaultName,
+        [parameter(Mandatory = $true)] $Name
     )
 
-    if(Get-AzureKeyVaultSecret -vaultName $VaultName -name $Name) {
+    if (Get-AzureKeyVaultSecret -vaultName $VaultName -name $Name) {
 
         $SecretValue = (get-AzureKeyVaultSecret -vaultName $VaultName -name $Name).SecretValueText
         Write-Verbose $SecretValue
@@ -77,17 +89,75 @@ function Get-Secret {
     return $SecretValue
 }
 
+function New-AzureKeyVault {
+    Param(
+        [parameter(Mandatory = $true)] [string]$TenantID,
+        [parameter(Mandatory = $true)] [string]$SubscriptionID,
+        [parameter(Mandatory = $true)] [string]$ResourceGroup,
+        [parameter(Mandatory = $true)] [string]$VaultName,
+        [parameter(Mandatory = $true)] [string]$Region,
+        [parameter(Mandatory = $true)] [string]$Company,
+        [parameter(Mandatory = $true)] [string]$Enviroment,
+        [parameter(Mandatory = $true)] [string]$ServicePrincipalName
+    )
+
+    $ClientSecret = ConvertTo-SecureString $( -join (0..64| ForEach-Object {[char][int]((65..90) + (97..122)  | Get-Random)})) -AsPlainText -Force
+    $AdminPassword = New-AdminPassword -Length 12
+
+    $creds = Get-Credential -Message "Login with a Microsoft or Work account"
+    Login-AzureRmAccount -TenantId $TenantID -Credential $creds
+    Select-AzureRmSubscription -SubscriptionId $SubscriptionID
+
+    Write-Host "Creating Service Principal $($ServicePrincipalName) for the deployment of the ARM template, making calls to the REST Api and Managing Azure Policies."
+    $ServicePrincipal = New-ServicePrincipal -Password $ClientSecret -DisplayName $ServicePrincipalName -HomePage "http://$(-join ((65..90) + (97..122) | Get-Random -Count 5 | ForEach-Object {[char]$_}))" -IdentifierUris "http://$(-join ((65..90) + (97..122) | Get-Random -Count 5 | ForEach-Object {[char]$_}))"
+    
+    Write-Host "Creating Azure Key Vault $($VaultName) in $($ResourceGroup) and authorize the Service Principal $($ServicePrincipalName) to get and set secrets"
+    New-KeyVault -VaultName $VaultName -ResourceGroup $ResourceGroup -Region $Region -Company $Company -Enviroment $Enviroment -ServicePrincipal $ServicePrincipal
+
+    Write-Host "Saving ClientSecret to the Azure Key Vault" 
+    New-Secret -VaultName $VaultName -Name $ServicePrincipalName -SecretValue $ClientSecret
+
+    Write-Host "Saving ClientSecret to the Azure Key Vault"
+    New-Secret -VaultName $VaultName -Name "LocalAdmin" -SecretValue $AdminPassword
+    
+}
+
+function Start-AzureARMDeployment {
+    Param(
+        [parameter(Mandatory = $true)] [string]$TenantID,
+        [parameter(Mandatory = $true)] [string]$SubscriptionID,
+        [parameter(Mandatory = $false)] [securestring]$AdminPassword,
+        [parameter(Mandatory = $true)] [string]$ServicePrincipal,
+        [parameter(Mandatory = $true)] [securestring]$ServicePrincipalPassword,
+        [parameter(Mandatory = $false)] [string]$ResourceGroup = "NO_RG",
+        [parameter(Mandatory = $false)] [string]$Region = "WestEurope", 
+        [parameter(Mandatory = $false)] [string]$ArmTemplate = ".\Templates\azuredeploy.json",
+        [parameter(Mandatory = $false)] [string]$ArmTemplateParameters = ".\Templates\azuredeploy.parameters.json",
+        [parameter(Mandatory = $false)] [string]$Company = "NO",
+        [parameter(Mandatory = $false)] [string]$Enviroment = "Test",
+        [parameter(Mandatory = $false)] [string]$DeployLabel
+    )
+
+    $DeployLabel = "$($ResourceGroup)_$($Enviroment)_$(Get-Date -Date (Get-Date -format "dd-MMM-yyyy HH:mm") -UFormat %s)"
+
+    $spCred = New-Object System.Management.Automation.PSCredential($ServicePrincipal, $ServicePrincipalPassword)
+    Add-AzureRmAccount -Credential $spCred -ServicePrincipal -TenantId $TenantID
+
+    Select-AzureRmSubscription -SubscriptionId $SubscriptionID
+
+    New-AzureRmResourceGroup -Name $ResourceGroup -Location $Region -Tag @{Company = $Company; Enviroment = $Enviroment} -Force
+    New-AzureRmResourceGroupDeployment -Name $DeployLabel -ResourceGroupName $ResourceGroup -TemplateFile $ArmTemplate -TemplateParameterFile $ArmTemplateParameters -Company $Company -AdminPassword $AdminPassword -AsJob -Verbose -Force
+    Set-AzurePolicyResourceGroup -PolicyName "Allowed Resources in ResourceGroup" -SubscriptionID $SubscriptionID -ApplicationID $ServicePrincipal -ClientSecret $ServicePrincipalPassword -ResourceGroup $ResourceGroup
+    Set-AzurePolicyResourceGroup -PolicyName "AllowedResources in Subscription" -SubscriptionID $SubscriptionID -ApplicationID $ServicePrincipal -ClientSecret $ServicePrincipalPassword -ResourceGroup
+    
+}
 function New-ServicePrincipal {
     [CmdletBinding()]
     Param(
-    [parameter(Mandatory=$true)]
-    [string]$DisplayName,
-    [parameter(Mandatory=$true)]
-    [securestring]$Password,
-    [parameter(Mandatory=$true)]
-    [string]$HomePage,
-    [parameter(Mandatory=$true)]
-    [string]$IdentifierUris
+        [parameter(Mandatory = $true)] [string]$DisplayName,
+        [parameter(Mandatory = $true)] [securestring]$Password,
+        [parameter(Mandatory = $true)] [string]$HomePage,
+        [parameter(Mandatory = $true)] [string]$IdentifierUris
     )
 
     $azureAdApplication = New-AzureRmADApplication -DisplayName $DisplayName -HomePage $HomePage -IdentifierUris $IdentifierUris -Password $Password
@@ -117,23 +187,17 @@ function New-ServicePrincipal {
 function New-KeyVault {
     [CmdletBinding()]
     Param(
-    [parameter(Mandatory=$true)]
-    [string]$VaultName,
-    [parameter(Mandatory=$true)]
-    [string]$ResourceGroup,
-    [parameter(Mandatory=$true)]
-    [string]$Region,
-    [parameter(Mandatory=$true)]
-    [string]$Company,
-    [parameter(Mandatory=$true)]
-    [string]$Enviroment,
-    [parameter(Mandatory=$true)]
-    [psobject]$ServicePrincipal 
+        [parameter(Mandatory = $true)] [string]$VaultName,
+        [parameter(Mandatory = $true)] [string]$ResourceGroup,
+        [parameter(Mandatory = $true)] [string]$Region,
+        [parameter(Mandatory = $true)] [string]$Company,
+        [parameter(Mandatory = $true)] [string]$Enviroment,
+        [parameter(Mandatory = $true)] [psobject]$ServicePrincipal 
     )
 
-    $ObjectID = $ServicePrincipal.Where({$_.Type -eq "ServicePrincipal"}).ID.ToString()
+    $ObjectID = $ServicePrincipal.Where( {$_.Type -eq "ServicePrincipal"}).ID.ToString()
     
-    New-AzureRmResourceGroup -Name $ResourceGroup -Location $Region -Tag @{Company=$Company; Enviroment=$Enviroment} -Force
+    New-AzureRmResourceGroup -Name $ResourceGroup -Location $Region -Tag @{Company = $Company; Enviroment = $Enviroment} -Force
     if (-not(Get-AzureRmKeyVault -VaultName $VaultName)) {
 
         New-AzureRmKeyVault -VaultName $VaultName -ResourceGroupName $ResourceGroup -Location $Region -EnabledForTemplateDeployment -Verbose 
@@ -143,14 +207,15 @@ function New-KeyVault {
 
     return $VaultName | Out-Null
 }
-function Set-AzurePolicy ($PolicyName, $SubscriptionID, $ApplicationID, $ClientSecret, $ResourceGroup) {
+
+function Set-AzurePolicyResourceGroup ($PolicyName, $SubscriptionID, $ApplicationID, $ClientSecret, $ResourceGroup) {
     [CmdletBinding()]
 
-    $json =@'
+    $json = @'
     {
         "properties": {
             "displayName": "Allowed resources",
-            "description": "This policy restrict the use of resources",
+            "description": "This policy restrict the use of resources with the ResourceGroup",
             "metadata": {
             "assignedBy": "Niels W."
             },
@@ -171,6 +236,38 @@ function Set-AzurePolicy ($PolicyName, $SubscriptionID, $ApplicationID, $ClientS
     $Secret = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
     
     $AzurePolicyManager = [AzureRestServiceManager]::new($SubscriptionID, $ApplicationID, $Secret)
-    $AzurePolicyManager.Put("https://management.azure.com/subscriptions/$($SubscriptionID)/resourceGroups/$($ResourceGroup)/providers/Microsoft.Authorization/policyAssignments/$($PolicyName)?api-version=2018-03-01",$json)
+    $AzurePolicyManager.Put("https://management.azure.com/subscriptions/$($SubscriptionID)/resourceGroups/$($ResourceGroup)/providers/Microsoft.Authorization/policyAssignments/$($PolicyName)?api-version=2018-03-01", $json)
+   
+}
+
+function Set-AzurePolicySubScription ($PolicyName, $SubscriptionID, $ApplicationID, $ClientSecret) {
+    [CmdletBinding()]
+
+    $json = @'
+    {
+        "properties": {
+            "displayName": "Allowed resources",
+            "description": "This policy restrict the use of resources with the ResourceGroup",
+            "metadata": {
+            "assignedBy": "Niels W."
+            },
+            "policyDefinitionId": "/subscriptions/8b32203d-d655-41b1-84a7-2f119b31dc8a/providers/Microsoft.Authorization/policyDefinitions/a08ec900-254a-4555-9bf5-e42af04b5c5c",
+            "parameters": {
+                "listOfResourceTypesAllowed": {
+                    "value": [
+                        "Microsoft.Compute/virtualMachines",
+                        "Microsoft.Network/publicIPAddresses",
+                        "Microsoft.Network/networkInterfaces"
+                    ]
+                }
+            }
+        }
+    }
+'@
+    $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($ClientSecret)
+    $Secret = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+    
+    $AzurePolicyManager = [AzureRestServiceManager]::new($SubscriptionID, $ApplicationID, $Secret)
+    $AzurePolicyManager.Put("https://management.azure.com/subscriptions/$($SubscriptionID)/resourceGroups/$($ResourceGroup)/providers/Microsoft.Authorization/policyAssignments/$($PolicyName)?api-version=2018-03-01", $json)
    
 }
